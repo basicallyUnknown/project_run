9fb012522d811e7c3a8bc35a460e544a41136247
Michael <bl33k@yandex.ru>
2025-06-06 20:37:36 +0300

    api users

--- app_run/views.py
+++ app_run/views.py
@@ -2,9 +2,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import viewsets
 from django.conf import settings
+from django.contrib.auth.models import User
 
 from app_run.models import Run
-from app_run.serializers import RunSerializer
+from app_run.serializers import RunSerializer, UserSerializer
 
 
 @api_view(['GET'])
@@ -15,6 +16,20 @@ def company_details(request):
     return Response(details)
 
 
-class RunViewSet(viewsets.ModelViewSet):
+class RunsViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+
+class UsersViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.filter(is_superuser=False)
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        qs = self.queryset
+        type = self.request.query_params.get("type", None)
+        if type == "coach":
+            qs = qs.filter(is_staff=True)
+        elif type == "athlete":
+            qs = qs.filter(is_staff=False)
+        return qs
--- project_run/urls.py
+++ project_run/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from app_run.views import company_details, RunViewSet
+from app_run.views import company_details, RunsViewSet, UsersViewSet
 from django.conf.urls.static import static
 from django.conf import settings
 
 router = DefaultRouter()
-router.register('api/runs', RunViewSet)
+router.register('api/runs', RunsViewSet)
+router.register('api/users', UsersViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
