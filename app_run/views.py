41ebf7a81055f847d09cbd8a9f640665bdd446a4
Michael <bl33k@yandex.ru>
2025-06-08 19:26:53 +0300

    stop and start run apis

--- app_run/views.py
+++ app_run/views.py
@@ -43,7 +43,7 @@ class UsersViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class RunStartAPI(APIView):
-    def get(self, request, id):
+    def post(self, request, id):
         run = get_object_or_404(Run, id=id)
         if run.status == 'init':
             run.status = 'in_progress'
@@ -55,7 +55,7 @@ class RunStartAPI(APIView):
 
 
 class RunStopAPI(APIView):
-    def get(self, request, id):
+    def post(self, request, id):
         run = get_object_or_404(Run, id=id)
         if run.status == 'in_progress':
             run.status = 'finished'
